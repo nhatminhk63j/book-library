a40af3e4ac7b968ee08297bd9da0eff186d774c5
Nhat Minh <nhatngo11a1@gmail.com>
2020-04-30 00:57:06 +0700

    upload file with multer/cloudinary && create schema user and book

---
+++ src/models/userModel.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+var userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String,
+    avatar: String
+})
+
+var User = mongoose.model('User', userSchema, 'users');
+
+module.exports = User;
