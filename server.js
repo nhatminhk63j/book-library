40148897b040c6ae080bfc0c8c072af39f33e62b
Nhat Minh <nhatngo11a1@gmail.com>
2020-04-29 18:41:53 +0700

    connect to database mongodb atlas

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ require('dotenv').config();
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+
+mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
+    if(err) console.log(err);
+    else console.log('Database is connected!');
+})
 
 app.get('/', (req, res, next) => res.send("Hello World!"))
 
